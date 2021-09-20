04e6c5ada2b56740a4488d7ea23a3989a1af5554
leonar.tambunan@gmail.com <leonar.tambunan@gmail.com>
2021-09-20 16:32:25 +0700

    m

--- src/main/java/id/co/bbw/Test.java
+++ src/main/java/id/co/bbw/Test.java
@@ -12,27 +12,60 @@ public class Test {
         File f = new File(userHome+"/"+"cfg/packager.xml");
         GenericPackager genericPackager = new GenericPackager(new FileInputStream(f));
 
-        ISOMsg isoMsg = new ISOMsg();
+        ISOMsg isoMsg = new ISOMsg("0200");
 
         isoMsg.setPackager(genericPackager);
-        String isoMessage = "";
-        isoMessage = "0200F23A400B88A0C008000000000400000016200889999999999930100000000000000010262216270541712216271026102660178000000008000000000390503905200000129491ALTOWBK MOB VIRTUAL ACCOUNT - BBW        JKT  ID3603600101000019991100110014254";
+//        String isoMessage = "";
+//        isoMessage = "0200F23A400B88A0C008000000000400000016200889999999999930100000000000000010262216270541712216271026102660178000000008000000000390503905200000129491ALTOWBK MOB VIRTUAL ACCOUNT - BBW        JKT  ID3603600101000019991100110014254";
+//
+//        isoMsg.unpack(isoMessage.getBytes());
+//
+//        for (int i = 0 ; i<=isoMsg.getMaxField();i++) {
+//            if (isoMsg.hasField(i)) {
+//                System.out.println(i+":'"+isoMsg.getString(i)+"'");
+//            }
+//        }
 
-        isoMsg.unpack(isoMessage.getBytes());
-
-        for (int i = 0 ; i<=isoMsg.getMaxField();i++) {
-            if (isoMsg.hasField(i)) {
-                System.out.println(i+":'"+isoMsg.getString(i)+"'");
-            }
-        }
-
+        isoMsg.set(2,"6048209999999999995");
+        isoMsg.set(3,"341000");
+        isoMsg.set(4,"000010000000");
+        isoMsg.set(7,"0904174227");
+        isoMsg.set(11,"095428");
+        isoMsg.set(12,"174226");
+        isoMsg.set(13,"0904");
+        isoMsg.set(15,"0619");
+        isoMsg.set(18,"6017");
+        isoMsg.set(22,"000");
+        isoMsg.set(26,"12");
         isoMsg.set(29,"C00000000");
         isoMsg.set(31,"C00000000");
+        isoMsg.set(32,"535");
+        isoMsg.set(33,"535");
+        isoMsg.set(35,"6048209999999999995=9999");
+        isoMsg.set(37,"463505877857");
+        isoMsg.set(41,"MB000045");
+        isoMsg.set(43,"0000000000000000000000000000000000000000");
+        isoMsg.set(49,"360");
+        isoMsg.set(50,"360");
+        isoMsg.set(52,"0000000000000000");
+        isoMsg.set(63,"1099999");
+        isoMsg.set(102,"999999999999");
+        isoMsg.set(104,"0000147610368872");
+
+        //isoMsg.unset(61);
 
 
         byte[] s = isoMsg.pack();
 
         System.out.println(new String(s));
+
+//        isoMsg.unpack(s);
+//
+//        for (int i = 0 ; i<=isoMsg.getMaxField();i++) {
+//            if (isoMsg.hasField(i)) {
+//                System.out.println(i+":'"+isoMsg.getString(i)+"'");
+//            }
+//        }
     }
 }
 
---
+++ src/main/java/id/co/bbw/controller/IsoController.java
@@ -0,0 +1,81 @@
+package id.co.bbw.controller;
+
+import org.apache.logging.log4j.util.Strings;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.q2.iso.QMUX;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/",
+        consumes = MediaType.TEXT_PLAIN_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        method = {RequestMethod.GET, RequestMethod.POST})
+public class IsoController {
+
+    private final QMUX qmux;
+
+    public IsoController(QMUX qmux) {
+        this.qmux = qmux;
+    }
+
+    @PostMapping("/isorequest")
+    public Map<String, String> isorequest(@RequestBody String request){
+        Map<String, String> result = new LinkedHashMap<>();
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonRequest;
+        try {
+            jsonRequest = (JSONObject) parser.parse(request);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            result.put("error","request contains malformed JSON");
+            return result;
+        }
+
+        System.out.println(request);
+        try {
+            ISOMsg msgRequest = new ISOMsg((String) jsonRequest.get("0"));
+
+            for (int x = 2; x<128;x++) {
+                String val = (String) jsonRequest.get(x+"");
+                if (Strings.isNotEmpty(val)) {
+                    msgRequest.set(x, val);
+                }
+            }
+
+            ISOMsg isoResponse = qmux.request(msgRequest, 10 * 1000); //10 seconds timeout
+
+            if(isoResponse == null) {
+                result.put("error", "timeout");
+                return result;
+            }
+
+            result.put("0",isoResponse.getMTI());
+
+            int j = isoResponse.getMaxField();
+            for (int i = 1; i<=j ; i++) {
+                String value =  (String) isoResponse.getValue(i);
+                if (Strings.isNotEmpty(value)) {
+                    result.put(i + "", value );
+                }
+            }
+        } catch (ISOException e) {
+            logger.error(e.getMessage());
+        }
+
+        return result;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(IsoController.class);
+}
+
